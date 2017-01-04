21ff776d5547c43159a9ab54508c8a89d0c88711
Henry Li <2298911475@qq.com>
2017-01-04 19:17:07 +0800

    achieve base move funcation

--- tvrecyclerview/src/main/java/app/com/tvrecyclerview/FlyBorderView.java
+++ tvrecyclerview/src/main/java/app/com/tvrecyclerview/FlyBorderView.java
@@ -1,35 +1,42 @@
 package app.com.tvrecyclerview;
 
 import android.content.Context;
-import android.graphics.Rect;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
 
 public class FlyBorderView extends View {
 
-    private static final String TAG = "FlyBorderView";
     private static final int TRAN_DUR_ANIM = 150;
+    private static final String TAG = "TvRecyclerView";
 
     private TvRecyclerView mTvRecyclerView;
-    private View mFocusView;
-    private View mSelectView;
-    private int mFocusedPos = -1;
-    private boolean mIsSelected = false;
+    private Scroller mScroller;
+
+    private float mScaleX;
+    private float mScaleY;
+
+    private boolean mIsDrawGetFocusAnim;
+    private boolean mIsNeedLayerTypeToSoftWare;
+
+    private int mLeftFocusBoundWidth;
+    private int mTopFocusBoundWidth;
+    private int mRightFocusBoundWidth;
+    private int mBottomFocusBoundWidth;
 
     public FlyBorderView(Context context) {
         super(context);
-        init();
-    }
-
-    public FlyBorderView(Context context, TvRecyclerView tvRecyclerView) {
-        super(context);
-        mTvRecyclerView = tvRecyclerView;
-        init();
-    }
-
-    private void init() {
-        setBackgroundResource(R.drawable.focus_2);
+        mScroller = new Scroller(context);
+        mIsDrawGetFocusAnim = false;
+        mLeftFocusBoundWidth = 0;
+        mTopFocusBoundWidth = 0;
+        mRightFocusBoundWidth = 0;
+        mBottomFocusBoundWidth = 0;
+        mScaleX = 0;
+        mScaleY = 0;
+        mIsNeedLayerTypeToSoftWare = true;
     }
 
     public TvRecyclerView getTvRecyclerView() {
@@ -38,75 +45,229 @@ public class FlyBorderView extends View {
 
     public void setTvRecyclerView(TvRecyclerView tvRecyclerView) {
         mTvRecyclerView = tvRecyclerView;
+        mIsNeedLayerTypeToSoftWare = mTvRecyclerView.getLayerType() == View.LAYER_TYPE_SOFTWARE;
     }
 
-    public int getFocusPos() {
-        return mFocusedPos;
+    public void setSelectPadding(int left, int top, int right, int bottom) {
+        mLeftFocusBoundWidth = left;
+        mTopFocusBoundWidth = top;
+        mRightFocusBoundWidth = right;
+        mBottomFocusBoundWidth = bottom;
     }
 
-    public void setFocusPos(int pos) {
-        if (pos >= 0) {
-            mFocusedPos = pos;
+    public void startFocusAnim() {
+        if (mTvRecyclerView != null) {
+            mTvRecyclerView.setLayerType(View.LAYER_TYPE_NONE, null);
+            View v = null;
+            int indexChild = mTvRecyclerView.getSelectedPosition();
+            if (indexChild >= 0 && indexChild < mTvRecyclerView.getChildCount()) {
+                v = mTvRecyclerView.getChildAt(indexChild);
+            }
+
+            if (v != null) {
+                mIsDrawGetFocusAnim = true;
+                mScroller.startScroll(0, 0, 100, 100, 300);
+                invalidate();
+            }
         }
     }
 
-    public View getFocusView() {
-        return mFocusView;
+    public void dismissFocus() {
+        mIsDrawGetFocusAnim = false;
+//        if (mTvRecyclerView != null) {
+//            mTvRecyclerView.invalidate();
+//        }
+        //invalidate();
+        mScroller.abortAnimation();
+        mScroller.forceFinished(true);
     }
 
-    public void setFocusView(View view, int position) {
-        if (mFocusedPos == -1 || (mFocusedPos != position && position >= 0)) {
-            mFocusedPos = position;
-            mFocusView = view;
-            runTranslateAnimation(mFocusView);
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            float scaleValue = mTvRecyclerView.getSelectedScaleValue();
+            mScaleX = ((scaleValue - 1) * mScroller.getCurrX()) / 100 + 1;
+            mScaleY = ((scaleValue - 1) * mScroller.getCurrY()) / 100 + 1;
+            invalidate();
+        } else {
+            if (mIsDrawGetFocusAnim) {
+                mIsDrawGetFocusAnim = false;
+                if (mTvRecyclerView != null) {
+                    mTvRecyclerView.setLayerType(mIsNeedLayerTypeToSoftWare ?
+                            View.LAYER_TYPE_SOFTWARE : View.LAYER_TYPE_NONE, null);
+                    invalidate();
+                }
+            }
         }
     }
 
-    public void setSelectView(View view, int position) {
-        if (mSelectView != view) {
-            mSelectView = view;
-            mFocusedPos = position;
-            mIsSelected = true;
-
-            runTranslateAnimation(mSelectView);
-            mIsSelected = false;
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        if (mTvRecyclerView != null && mTvRecyclerView.hasFocus()) {
+            drawGetFocusScaleAnim(canvas);
+            //drawFocusMoveAnim(canvas);
+            drawFocus(canvas);
         }
     }
 
-    private void runTranslateAnimation(View toView) {
-        Rect fromRect = findLocationWithView(this);
-        Rect toRect = findLocationWithView(toView);
-        int x = toRect.left - fromRect.left;
-        int y = toRect.top - fromRect.top;
+    private void drawGetFocusScaleAnim(Canvas canvas) {
+        if (mIsDrawGetFocusAnim) {
+            View itemView = null;
+            int indexChild = mTvRecyclerView.getSelectedPosition();
+            if (indexChild >= 0 && indexChild < mTvRecyclerView.getChildCount()) {
+                itemView = mTvRecyclerView.getChildAt(indexChild);
+            }
 
-        int deltaX = (toView.getWidth() - getWidth()) / 2;
-        int deltaY = (toView.getHeight() - getHeight()) / 2;
+            if (itemView == null) {
+                return;
+            }
 
-        x = x + deltaX;
-        y = y + deltaY;
+            int itemWidth = itemView.getWidth();
+            int itemHeight = itemView.getHeight();
 
-        float scaleX = toView.getWidth() / getWidth();
-        float scaleY = toView.getHeight() / getHeight();
-        flyWhiteBorder(x, y, scaleX, scaleY);
+            int[] location = new int[2];
+            itemView.getLocationInWindow(location);
+            int[] drawLocation = new int[2];
+            getLocationInWindow(drawLocation);
 
-    }
+            // draw focus image
+            Drawable drawableFocus = mTvRecyclerView.getDrawableFocus();
+            if (drawableFocus != null) {
+                int focusWidth = itemWidth + mLeftFocusBoundWidth + mRightFocusBoundWidth;
+                int focusHeight = itemHeight + mTopFocusBoundWidth + mBottomFocusBoundWidth;
 
-    private void flyWhiteBorder(float x, float y, float scaleX, float scaleY) {
-        int duration = TRAN_DUR_ANIM;
-        if (mIsSelected) {
-            duration = 0;
+                canvas.save();
+                canvas.translate(location[0] - mLeftFocusBoundWidth, location[1] - drawLocation[1] - mTopFocusBoundWidth);
+                canvas.scale(mScaleX, mScaleY, itemWidth / 2, itemHeight / 2);
+
+                drawableFocus.setBounds(0, 0, focusWidth, focusHeight);
+                drawableFocus.draw(canvas);
+                canvas.restore();
+            }
+
+            // draw item view
+            canvas.save();
+            canvas.translate(location[0], location[1]);
+            canvas.scale(mScaleX, mScaleY, itemWidth / 2, itemHeight / 2);
+            itemView.draw(canvas);
+            canvas.restore();
         }
-
-        animate().scaleX(scaleX).scaleY(scaleY).setDuration(duration).
-                setInterpolator(new DecelerateInterpolator()).start();
-        animate().translationX(x).translationY(y).setDuration(duration).
-                setInterpolator(new DecelerateInterpolator()).start();
     }
 
-    public Rect findLocationWithView(View view) {
-        ViewGroup root = (ViewGroup) this.getParent();
-        Rect rect = new Rect();
-        root.offsetDescendantRectToMyCoords(view, rect);
-        return rect;
+    private void drawFocusMoveAnim(Canvas canvas) {
+        if (mTvRecyclerView.mIsDrawFocusMoveAnim) {
+            View curView = mTvRecyclerView.getChildAt(mTvRecyclerView.getSelectedPosition());
+            View nextView = mTvRecyclerView.getNextFocusView();
+
+            if (nextView != null && curView != null) {
+                int[] locationDrawLayout = new int[2];
+                this.getLocationInWindow(locationDrawLayout);
+
+                int[] location = new int[2];
+                nextView.getLocationInWindow(location);
+                int nextLeft = location[0];
+                int nextTop = location[1] - locationDrawLayout[1];
+                int nextWidth = nextView.getWidth();
+                int nextHeight = nextView.getHeight();
+
+                curView.getLocationInWindow(location);
+                int curLeft = location[0];
+                int curTop = location[1] - locationDrawLayout[1];
+                int curWidth = curView.getWidth();
+                int curHeight = curView.getHeight();
+
+                float animScale = mTvRecyclerView.getFocusMoveAnimScale();
+                float focusLeft = curLeft + (nextLeft - curLeft) * animScale;
+                float focusTop = curTop + (nextTop - curTop) * animScale;
+                float focusWidth = curWidth + (nextWidth - curWidth) * animScale;
+                float focusHeight = curHeight + (nextHeight - curHeight) * animScale;
+
+                // draw focus image
+                Drawable drawableFocus = mTvRecyclerView.getDrawableFocus();
+                float scaleValue = mTvRecyclerView.getSelectedScaleValue();
+                if (drawableFocus != null) {
+                    canvas.save();
+                    canvas.translate(
+                            focusLeft - (scaleValue - 1) / 2 * focusWidth,
+                            focusTop - (scaleValue - 1) / 2 * focusHeight);
+                    canvas.scale(scaleValue, scaleValue, 0, 0);
+
+                    drawableFocus.setBounds(
+                            0 - mLeftFocusBoundWidth,
+                            0 - mTopFocusBoundWidth,
+                            (int) (focusWidth + mRightFocusBoundWidth),
+                            (int) (focusHeight + mBottomFocusBoundWidth));
+                    drawableFocus.draw(canvas);
+                    canvas.restore();
+                }
+
+                // draw next item view
+                canvas.save();
+                canvas.translate(
+                        focusLeft - (scaleValue - 1) / 2 * focusWidth,
+                        focusTop - (scaleValue - 1) / 2 * focusHeight);
+                canvas.scale(
+                        (scaleValue * focusWidth) / nextWidth,
+                        (scaleValue * focusHeight) / nextHeight,
+                        0,
+                        0);
+                canvas.saveLayerAlpha(new RectF(0, 0, this.getWidth(), this.getHeight()),
+                        (int) (0xFF * animScale), Canvas.ALL_SAVE_FLAG);
+                nextView.draw(canvas);
+                canvas.restore();
+
+                // draw current item view
+                canvas.save();
+                canvas.translate(
+                        focusLeft - (scaleValue - 1) / 2 * focusWidth,
+                        focusTop - (scaleValue - 1) / 2 * focusHeight);
+                canvas.scale(
+                        (scaleValue * focusWidth) / curWidth,
+                        (scaleValue * focusHeight) / curHeight,
+                        0,
+                        0);
+                canvas.saveLayerAlpha(new RectF(0, 0, this.getWidth(), this.getHeight()),
+                        (int) (0xFF * (1 - animScale)), Canvas.ALL_SAVE_FLAG);
+                curView.draw(canvas);
+                canvas.restore();
+            }
+        }
+    }
+
+    private void drawFocus(Canvas canvas) {
+        if (!mIsDrawGetFocusAnim) {
+            View itemView = mTvRecyclerView.getChildAt(mTvRecyclerView.getSelectedPosition());
+            if (itemView != null) {
+                int[] itemLocation = new int[2];
+                itemView.getLocationInWindow(itemLocation);
+
+                int itemWidth = itemView.getWidth();
+                int itemHeight = itemView.getHeight();
+                float scaleValue = mTvRecyclerView.getSelectedScaleValue();
+                float itemPositionX = itemLocation[0] - (scaleValue - 1) / 2 * itemWidth;
+                float itemPositionY = itemLocation[1] - (scaleValue - 1) / 2 * itemHeight;
+
+                //draw focus image
+                Drawable drawableFocus = mTvRecyclerView.getDrawableFocus();
+                int drawWidth = itemWidth + mLeftFocusBoundWidth + mRightFocusBoundWidth;
+                int drawHeight = itemHeight + mTopFocusBoundWidth + mBottomFocusBoundWidth;
+                float drawPositionX = itemPositionX - scaleValue * mLeftFocusBoundWidth;
+                float drawPositionY = itemPositionY - scaleValue * mTopFocusBoundWidth;
+                canvas.save();
+                canvas.translate(drawPositionX, drawPositionY);
+                canvas.scale(scaleValue, scaleValue, 0, 0);
+                drawableFocus.setBounds(0, 0, drawWidth, drawHeight);
+                drawableFocus.draw(canvas);
+                canvas.restore();
+
+                // draw item view
+                canvas.save();
+                canvas.translate(itemPositionX, itemPositionY);
+                canvas.scale(scaleValue, scaleValue, 0, 0);
+                itemView.draw(canvas);
+                canvas.restore();
+            }
+        }
     }
 }
--- tvrecyclerview/src/main/java/app/com/tvrecyclerview/TvRecyclerView.java
+++ tvrecyclerview/src/main/java/app/com/tvrecyclerview/TvRecyclerView.java
@@ -14,15 +14,31 @@ import android.view.FocusFinder;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 public class TvRecyclerView extends RecyclerView {
 
-    private static final String TAG = "TvRecyclerView";
+    public static final String TAG = "TvRecyclerView";
 
     private FlyBorderView mFlyBorderView;
+
     protected Drawable mDrawableFocus;
-    protected boolean mIsDrawFocusMoveAnim;
-    protected float mFocusMoveAnimScale;
+    public boolean mIsDrawFocusMoveAnim;
+    protected float mSelectedScaleValue;
+    private float mFocusMoveAnimScale;
+
+    private int mSelectedPosition;
+    private View mNextFocused;
+    private boolean mInLayout;
+
+    private int mFocusFrameLeft;
+    private int mFocusFrameTop;
+    private int mFocusFrameRight;
+    private int mFocusFrameBottom;
+    private boolean mReceivedInvokeKeyDown;
+    private View mSelectedItem;
+    private OnItemStateListener mItemStateListener;
+    private Scroller mScrollerFocusMoveAnim;
 
     public TvRecyclerView(Context context) {
         this(context, null);
@@ -39,52 +55,68 @@ public class TvRecyclerView extends RecyclerView {
 
     private void init() {
         mDrawableFocus = ContextCompat.getDrawable(getContext(), R.drawable.focus_2);
+        mScrollerFocusMoveAnim = new Scroller(getContext());
         mIsDrawFocusMoveAnim = false;
-        mFocusMoveAnimScale = 1;
+        mReceivedInvokeKeyDown = false;
+        mSelectedScaleValue = 1.04f;
+        mFocusMoveAnimScale = 1.0f;
+        mSelectedPosition = 0;
+        mNextFocused = null;
+        mInLayout = false;
+
+        mFocusFrameLeft = 32;
+        mFocusFrameTop = 22;
+        mFocusFrameRight = 32;
+        mFocusFrameBottom = 43;
+        setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
     }
 
+    private void addFlyBorderView(Context context) {
+        if (mFlyBorderView == null) {
+            mFlyBorderView = new FlyBorderView(context);
+            ((Activity) context).getWindow().addContentView(mFlyBorderView,
+                    new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+            mFlyBorderView.setSelectPadding(mFocusFrameLeft, mFocusFrameTop,
+                    mFocusFrameRight, mFocusFrameBottom);
+        }
+    }
+
+    public void setItemStateListener(OnItemStateListener listener) {
+        mItemStateListener = listener;
+    }
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         addFlyBorderView(getContext());
     }
 
-    private void addFlyBorderView(Context context) {
-        if (null == mFlyBorderView) {
-            mFlyBorderView = new FlyBorderView(context, this);
-            ((Activity)context).getWindow().addContentView(mFlyBorderView,
-                    new LayoutParams(200, 200));
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+        mFlyBorderView.setTvRecyclerView(this);
+        if (gainFocus) {
+            mFlyBorderView.bringToFront();
         }
-        Log.i(TAG, "addFlyBorderView: [ " + hashCode() + " ]");
+        if (gainFocus && !mInLayout) {
+            mFlyBorderView.startFocusAnim();
+        } else {
+            mFlyBorderView.dismissFocus();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mInLayout = true;
+        super.onLayout(changed, l, t, r, b);
+        mInLayout = false;
     }
 
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
-        if (mFlyBorderView != null) {
+        if (mFlyBorderView != null && mFlyBorderView.getTvRecyclerView() != null) {
             mFlyBorderView.invalidate();
-        }
-        Log.i(TAG, "dispatchDraw: [ " + hashCode() + " ]");
-    }
-
-    @Override
-    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
-        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
-        Log.i(TAG, "onFocusChanged: gainFocus==" + gainFocus + "==direction==" + direction);
-        if (gainFocus) {
-            mFlyBorderView.bringToFront();
-            int focusPos = mFlyBorderView.getFocusPos();
-            if (focusPos < 0) {
-                ViewGroup.LayoutParams layoutParams = mFlyBorderView.getLayoutParams();
-                layoutParams.width = getChildAt(0).getWidth() + 100;
-                layoutParams.height = getChildAt(0).getHeight() + 100;
-                mFlyBorderView.setLayoutParams(layoutParams);
-                focusPos = 0;
-            }
-            mFlyBorderView.setFocusView(getChildAt(focusPos), focusPos);
-            mFlyBorderView.setVisibility(View.VISIBLE);
-        } else {
-            mFlyBorderView.setVisibility(View.INVISIBLE);
+            Log.i(TAG, "dispatchDraw: ==================================");
         }
     }
 
@@ -92,22 +124,134 @@ public class TvRecyclerView extends RecyclerView {
     public boolean dispatchKeyEvent(KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             int keyCode = event.getKeyCode();
-            View nextFocused = null;
+            if (mNextFocused == null) {
+                mSelectedItem = getChildAt(mSelectedPosition);
+            }
             if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
-                nextFocused = FocusFinder.getInstance().findNextFocus(this, findFocus(), View.FOCUS_LEFT);
+                mNextFocused = FocusFinder.getInstance().findNextFocus(this, mSelectedItem, View.FOCUS_LEFT);
             } else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
-                nextFocused = FocusFinder.getInstance().findNextFocus(this, findFocus(), View.FOCUS_RIGHT);
+                mNextFocused = FocusFinder.getInstance().findNextFocus(this, mSelectedItem, View.FOCUS_RIGHT);
             } else if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
-                nextFocused = FocusFinder.getInstance().findNextFocus(this, findFocus(), View.FOCUS_UP);
+                mNextFocused = FocusFinder.getInstance().findNextFocus(this, mSelectedItem, View.FOCUS_UP);
             } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
-                nextFocused = FocusFinder.getInstance().findNextFocus(this, findFocus(), View.FOCUS_DOWN);
+                mNextFocused = FocusFinder.getInstance().findNextFocus(this, mSelectedItem, View.FOCUS_DOWN);
             }
-
-            if (nextFocused != null) {
-                mFlyBorderView.setFocusView(nextFocused, getChildAdapterPosition(nextFocused));
-            }
-
         }
         return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case KeyEvent.KEYCODE_DPAD_UP:
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                if (move()) {
+                    return true;
+                }
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+                mReceivedInvokeKeyDown = true;
+                break;
+
+            default:
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER: {
+                if (mReceivedInvokeKeyDown) {
+                    if ((getAdapter() != null) && (mSelectedItem != null)) {
+                        if (mItemStateListener != null) {
+                            mItemStateListener.onItemViewClick(mSelectedItem, mSelectedPosition);
+                        }
+                    }
+                }
+                mReceivedInvokeKeyDown = false;
+                return true;
+            }
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScrollerFocusMoveAnim.computeScrollOffset()) {
+            if (mIsDrawFocusMoveAnim) {
+                mFocusMoveAnimScale = ((float) (mScrollerFocusMoveAnim.getCurrX())) / 100;
+                Log.i(TAG, "computeScroll: ==================mFocusMoveAnimScale=" + mFocusMoveAnimScale);
+            }
+            postInvalidate();
+        } else {
+            if (mIsDrawFocusMoveAnim) {
+                mSelectedItem = mNextFocused;
+                mSelectedPosition = indexOfChild(mSelectedItem);
+                mIsDrawFocusMoveAnim = false;
+                setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                postInvalidate();
+            }
+        }
+    }
+
+    private boolean move() {
+        if (mNextFocused == null) {
+            return false;
+        } else {
+            Log.i(TAG, "move: ========================next view pos===========" + indexOfChild(mNextFocused));
+            startFocusMoveAnim();
+            return true;
+        }
+    }
+
+    private void startFocusMoveAnim() {
+        setLayerType(View.LAYER_TYPE_NONE, null);
+        mIsDrawFocusMoveAnim = true;
+        mScrollerFocusMoveAnim.startScroll(0, 0, 100, 100, 200);
+        invalidate();
+    }
+
+    public int getSelectedPosition() {
+        return mSelectedPosition;
+    }
+
+    public float getSelectedScaleValue() {
+        return mSelectedScaleValue;
+    }
+
+    public Drawable getDrawableFocus() {
+        return mDrawableFocus;
+    }
+
+    public View getNextFocusView() {
+        return mNextFocused;
+    }
+
+    public float getFocusMoveAnimScale() {
+        return mFocusMoveAnimScale;
+    }
+
+    public void setSelectPadding(int left, int top, int right, int bottom) {
+        mFocusFrameLeft = left;
+        mFocusFrameTop = top;
+        mFocusFrameRight = right;
+        mFocusFrameBottom = bottom;
+
+        if (null != mFlyBorderView) {
+            mFlyBorderView.setSelectPadding(mFocusFrameLeft, mFocusFrameTop,
+                    mFocusFrameRight, mFocusFrameBottom);
+        }
+    }
+
+    public interface OnItemStateListener {
+        void onItemViewClick(View view, int position);
+        void onItemViewFocused(View view, int position);
     }
 }
